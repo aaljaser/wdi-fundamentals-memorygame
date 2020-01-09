256b36f76f5f7e0850ad4f897718b190215b8207
aaljaser <56267638+aaljaser@users.noreply.github.com>
2020-01-09 16:51:47 +0300

    created game logic

--- memorygame/main.js
+++ memorygame/main.js
@@ -1,3 +1,23 @@
-let cardOne,cardTwo,cardThree,cardFour;
-cardOne="queen";cardTwo="king";cardThree="queen";cardFour="king";
-console.log("User flipped " + cardOne);
+
+let card =["queen","queen","king","king"];
+let cardsInPlay=[];
+let cardOne=card[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped "+ " "+cardsInPlay);
+
+let cardTwo=card[2];
+cardsInPlay.push(cardTwo);
+    if (cardsInPlay.length===2)
+    {
+        if(cardsInPlay[0]===cardsInPlay[1])
+        {
+            alert("You found a match!");
+        }
+
+        else
+        {
+                    alert("Sorry, try again.");
+        }
+        
+    }
